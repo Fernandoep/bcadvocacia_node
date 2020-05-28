2be00c2a13dba16363127f7c25805faaacd2f202
Fernando Paranhos <fernandoeparanhos@gmail.com>
2020-05-28 12:14:26 -0300

    Maiscula

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/login', function (req, res) {
         from: 'brunacarvalho@bcadvocacia.com',
         to: 'fernandoeparanhos@gmail.com',
         subject: req.body.nome,
-        text: `telefone: ${req.body.telefone}\n Mensagem: ${req.body.conteudo}\n e-mail: ${req.body.email}`
+        text: ` Telefone: ${req.body.telefone}\n Mensagem: ${req.body.conteudo}\n E-mail: ${req.body.email}`
     }
     transporter.sendMail(mailOptions, function(err, info){
         if(err){
