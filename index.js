5ac3429a6900fd5310e1271c63c4e14fd92a5e0c
Fernando Paranhos <fernandoeparanhos@gmail.com>
2020-05-28 11:22:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+let app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.hostinger.com.br',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'brunacarvalho@bcadvocacia.com',
+        pass: 'paranhos2020'
+   }
+});
+
+
+
+
+app.get('/', (req, res) => res.send('servidor rodando'));
+
+app.post('/login', function (req, res) {
+    console.log('Body ===>', req.body);
+    var mailOptions = {
+        from: 'brunacarvalho@bcadvocacia.com',
+        to: 'fernandoeparanhos@gmail.com',
+        subject: req.body.nome,
+        text: `Cliente: ${req.body.conteudo}, telefone: ${req.body.telefone} Mensagem: ${req.body.conteudo} e-mail: ${req.body.email}`
+    }
+    transporter.sendMail(mailOptions, function(err, info){
+        if(err){
+            res.send(err);
+        }else{
+            res.send('Mensagem enviada com sucesso');
+       
+        }
+    });
+
+})
+
+let port = process.env.PORT || 5000 ;
+app.listen(port, () => console.log('servidor rodando'));
